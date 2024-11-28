73b75192d98045fc667f36132f191b6db31adbf3
Matija Jandri <zudemain@gmail.com>
2024-11-28 09:21:17 +0100

    Discord ping sound als Objekt Verarscher

---
+++ Verarscher.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class Verarscher {
+
+    public Verarscher() throws InterruptedException {
+        Musik discord = new Musik();
+        Random random = new Random();
+        while(true){
+            Thread.sleep(random.nextInt(250000, 120000));
+            discord.PlayMusic("discord.wav");
+        }
+    }
+}
